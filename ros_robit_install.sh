b2230a9612b55935ab2bb91367a8024458ab71bb
이명진 <66550892+mjlee111@users.noreply.github.com>
2023-02-07 17:00:07 +0900

    Update ros_robit_install.sh

--- ros_robit_install.sh
+++ ros_robit_install.sh
@@ -70,7 +70,7 @@ sh -c "echo \"alias cs='cd ~/catkin_ws/src'\" >> ~/.bashrc"
 sh -c "echo \"alias cm='cd ~/catkin_ws && catkin_make'\" >> ~/.bashrc"
 
 sh -c "echo \"source /opt/ros/melodic/setup.bash\" >> ~/.bashrc"
-sh -c "echo \"source ~/melodic/devel/setup.bash\" >> ~/.bashrc"
+sh -c "echo \"source ~/catkin_ws/devel/setup.bash\" >> ~/.bashrc"
 echo ""
 echo ""
 echo ""
@@ -94,9 +94,12 @@ echo ""
 echo ""
 
 echo "[RO:BIT] making catkin_workspace"
+cd
 mkdir catkin_ws
 cd catkin_ws
 mkdir src
+cd 
+sudo chmod 777 -R ~/.ros/
 echo ""
 echo ""
 echo ""
